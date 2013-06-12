d2a0d58eca9b2e7c3ca84398017a295a6ba0b7ff
leo-the-manic <manicleo@gmail.com>
2013-06-12 11:55:30 -0400

    Remove duplicated database entries

--- demo/settings.py
+++ demo/settings.py
@@ -12,21 +12,14 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'demo.sqlite'  # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.' + DATABASE_ENGINE,
-        'NAME': DATABASE_NAME,
-        'USER': DATABASE_USER,
-        'PASSWORD': DATABASE_PASSWORD,
-        'HOST': DATABASE_HOST,
-        'PORT': DATABASE_PORT,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'demo.sqlite',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
